1ff409f11eb6068efae66a9dd95355305564958c
Ojas Srivastava <ojas1807@gmail.com>
2025-01-18 15:10:48 -0500

    Added OpenAI

--- app/analysis/page.tsx
+++ app/analysis/page.tsx
@@ -1,4 +1,20 @@
+
 import AnalPage from "@/components/anal";
+
+import OpenAI from "openai";
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API});
+
+const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+            role: "user",
+            content: "Write a haiku about recursion in programming.",
+        },
+    ],
+});
+
 
 
 export default function Analysis() {
