fed437bb852958e4b252cde60cf11965e38753d6
Ojas Srivastava <ojas1807@gmail.com>
2025-01-18 01:29:33 -0500

    Pricing

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -3,7 +3,7 @@ import { title } from "@/components/primitives";
 export default function PricingPage() {
   return (
     <div>
-      <h1 className={title()}>Pricing</h1>
+      <h1 className={title()}>It's free forever, lol!</h1>
     </div>
   );
 }
